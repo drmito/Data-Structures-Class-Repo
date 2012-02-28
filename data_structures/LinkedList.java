6e357cfaa980a7f1afee96076d0d95b0a58d7d43
Guillermo Sanz-Berney <drmito@me.com>
2012-02-28 12:07:18 -0800

    new commit

--- data_structures/LinkedList.java
+++ data_structures/LinkedList.java
@@ -4,113 +4,130 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class LinkedList<E> implements ListI<E> {
-	class IteratorHelper <E> implements Iterator<E> {
-		}
-	public interatorHelper() {
-		public boolean hasNext() {
-		}
-		public E Next{
-			if (!= hasNext)
-				throw new NoSuchElementException(); 
-		}
-		public void remove {
-			throw new UnsupportedOperationException();
-		
-		}
-	}
-	ListI implements iterable<E> {
-		@SuppressWarnings("rawtypes")
-		public Iterator Iterator(){
-			return IteratorHelper();
-	}
-	class Node<E> {
-		E data;
-		Node<E> next;
-		
-		public Node(E obj); {
-			data=obj;
-			next=null;
-		}
-		private Node<E> head, tail;
-		private int counter;
-		public LinkedList() {
-			head=null;
-			counter=0;
-		}
-		public LinkedList() {
-			head=tail=null;
-		}
-	}
 
-	@Override
+@SuppressWarnings("hiding")
+class Node<E> {
+	E data;
+	Node<E> next;
+	
+	public Node(E obj) {
+		data=obj;
+		next=null;
+	}
+}
+	private Node<E> head, tail;
+	private int counter;
+	public LinkedList() {
+		head=null;
+		tail=null;
+		counter=0;
+	}
 	public void addFirst(E obj) {
-		// TODO Auto-generated method stub
-		
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		Node<E> node = new Node(obj);
+		if(head==null)
+			tail=node;
+		node.next=head;
+		counter++;
+		head=node;
 	}
-
-	@Override
 	public void addLast(E obj) {
-		// TODO Auto-generated method stub
-		
+		if(head==null)
+			addFirst(obj);
+		else{
+			Node<E> newnode = new Node<E>(obj);
+			tail.next=newnode;
+			tail=newnode;
+			counter++;
+		}
 	}
-
-	@Override
 	public E removeFirst() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
+			if(head==null)
+				return null;
+			E tmp = head.data;
+			head = head.next;
+			counter--;
+			return tmp;
+		}
 	public E removeLast() {
-		// TODO Auto-generated method stub
-		return null;
+		if(head==null){return null;}
+		if(head.next==null){return removeFirst();}
+		Node<E> current = head;
+		Node<E> previous = null;
+		while(current.next != null){
+			previous=current;
+			current=current.next;
+		}
+		tail=previous;
+		previous.next=null;
+		counter--;
+		return current.data;
 	}
 
-	@Override
 	public E peekFirst() {
-		// TODO Auto-generated method stub
-		return null;
+		if(head==null)
+			return null;
+		return tail.data;
 	}
 
-	@Override
 	public E peekLast() {
-		// TODO Auto-generated method stub
-		return null;
+		if(head==null)
+			return head.data;
+		return tail.data;
 	}
 
-	@Override
 	public void makeEmpty() {
-		// TODO Auto-generated method stub
-		
+		head = null;
+		tail = null;
+		counter = 0;
 	}
 
-	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return (head==null);
 	}
 
-	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		return (head != null);
 	}
 
-	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return counter;
 	}
 
-	@Override
+	@SuppressWarnings("unchecked")
 	public boolean contains(E obj) {
-		// TODO Auto-generated method stub
+	Node<E> tmp = head;
+	while(tmp != null){
+		if(((Comparable<E>) tmp.data).compareTo(obj) == 0)
+			return true;
+		tmp=tmp.next;
+	}
 		return false;
 	}
 
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Iterator iterator() {
+		return new IteratorHelper();
+	}	
+
+public class IteratorHelper implements Iterator<E> {
+		Node<E> position;
+	
+		public IteratorHelper(){
+			position = (Node<E>) head;
 	}
+		public boolean hasNext() {
+			return position != null;
+		}
+		public E next(){
+			if (!hasNext())
+				throw new NoSuchElementException(); 
+		E tmp = position.data;
+		position = position.next;
+		return tmp;
+		}
+		public void remove(){
+			throw new UnsupportedOperationException();	
+	}
+}
 }
